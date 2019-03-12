7773bdb2486c3f2b1d5b76f15580ed9ff0e7af56
Gavin Ives <gavinives@live.com>
2019-03-12 19:30:27 -0400

    Task

--- Task Manager Project/Program.cs
+++ Task Manager Project/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task_Manager_Project
 {
-    class app
+    class App
     {
-        
+        public void Run()
         {
+            List<string> taskList = ReadListFromFile();
+            PrintTaskList(taskList);
+            
+
         }
+
+        private void PrintTaskList(List<string> taskList)
+        {
+            foreach (var t in taskList)
+
+                Console.WriteLine(t);
+        }
+
+        private List<string> ReadListFromFile()
+        {
+            var taskList = new List<string>();
+
+            try
+            {
+
+                using (StreamReader sr = new StreamReader(@"C:\Users\Default\Documents\TaskManager.txt"))
+                {
+                    var input = sr.ReadLine();
+                    taskList.Add(input);
+                }
+            }
+            catch (FileNotFoundException)
+            { ; }
+
+            return taskList;
+        }
+            
     }
 }
